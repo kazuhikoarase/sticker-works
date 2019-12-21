bfee5ffb7382dbb1ae2fde6552d764070ecaf545
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2019-12-21 18:39:15 +0900

    commit

--- index.js
+++ index.js
@@ -21,9 +21,13 @@ var requestListener = (req,res) => {
   if (contentType) {
     res.writeHead(200, { 'Content-Type': contentType });
     res.end(fs.readFileSync(__dirname + '/docs' + url, 'UTF-8') );
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/html' });
+    res.end('<h1>Not Found</h1>');
   }
 };
 
+var port = 8080;
 var server = http.createServer(requestListener);
-server.listen(8080);
-console.log('server started.');
+server.listen(port);
+console.log('server started at port ' + port);
