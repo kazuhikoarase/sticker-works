f27fb98e488b459cae422540de2eddc0c97983d0
Kazuhiko Arase <kazuhiko.arase@gmail.com>
2020-07-23 01:05:16 +0900

    commit

--- docs/sheets/sketch.js
+++ docs/sheets/sketch.js
@@ -90,71 +90,9 @@ new Vue({
   },
   computed: {
     sheets : function() {
-
-      var getId = function(i) {
-        var id = '' + i;
-        while (id.length < 2) {
-          id = '0' + id;
-        }
-        return id;
-      };
-
-      var config = this.config;
-      var stickerWidth = this.stickerWidth;
-      var stickerHeight = this.stickerHeight;
-
-      // if auto calc, temporary set the paper size.
-      if (stickerWidth <= 0) {
-        stickerWidth = config.paperWidth;
-      }
-      if (stickerHeight <= 0) {
-        stickerHeight = config.paperHeight;
-      }
-
-      if (config.rotate) {
-        // swap
-        var tmpW = stickerWidth;
-        stickerWidth = stickerHeight;
-        stickerHeight = tmpW;
-      }
-
-      // calc position.
-      var strings = this.strings.slice(); // copy of strings.
-      var x = config.marginLeft;
-      var y = config.marginTop;
-      var sheets = [];
-
-      while (strings.length > 0) {
-        var stickers = [];
-        while (strings.length > 0) {
-
-          var qrs = [];
-          while (strings.length > 0 && qrs.length < config.qrMarginLeft.length) {
-            qrs.push({ data: strings.shift(),
-                      x: config.qrMarginLeft[qrs.length].coarse +
-                        config.qrMarginLeft[qrs.length].fine / 10,
-                      y: config.qrMarginTop.coarse +
-                        config.qrMarginTop.fine / 10 });
-          }
-
-          stickers.push({ qrs: qrs, x: x, y: y });
-          x += stickerWidth + config.hGap;
-          if (x + stickerWidth >
-              config.paperWidth - config.marginRight) {
-            // next line.
-            x = config.marginLeft;
-            y += stickerHeight + config.vGap;
-            if (y + stickerHeight >
-                config.paperHeight - config.marginBottom) {
-              // next sheet.
-              y = config.marginTop;
-              break;
-            }
-          }
-        }
-        sheets.push({ id: getId(sheets.length + 1), stickers: stickers });
-      }
-      return sheets;
+      return stickerUtil.getSheets(this.config, this.stickerWidth, this.stickerHeight,
+          this.strings.slice() // copy of strings.
+      );
     },
     svgBgStyle: function() {
       var config = this.config;
